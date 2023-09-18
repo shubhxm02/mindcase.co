cdc820ec96238c596ccddcb7a5b81b10dcebd0b3
Shubham Gulati <shubham53.sg@gmail.com>
2023-09-19 01:07:58 +0530

    Revert "pass children as components"
    This reverts commit f9c569e767b04e318cb9606fee99760bbcb27886.

--- src/app/_layout/dashboard-layout.tsx
+++ src/app/_layout/dashboard-layout.tsx
@@ -5,25 +5,16 @@ import { Box } from "@mui/material";
 
 import { DashboardClient } from "./dashboard-client";
 
-const DashboardLayoutClient = async ({ children }: { children: React.ReactNode }) => {
-  // const session = await getSession();
-  const session = null;
-  return (
-    <DashboardClient children={children} session={session} />
-  );
-}
-
 export default function DashboardLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
+
+  const session = null;
+  // const session = await getSession();
+
   return (
     <>
-      {/* <DashboardSidebar onClose={() => setSidebarOpen(false)} open={isSidebarOpen} /> */}
-      {/* <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} /> */}
-      <DashboardLayoutClient>
+      <DashboardClient session={session}>
         {children}
-      </DashboardLayoutClient>
-      {/* <DashboardLayoutRoot> */}
-      {/* <Box sx={{ display: "flex", flex: "1 1 auto", flexDirection: "column", width: "100%" }}>{children}</Box> */}
-      {/* </DashboardLayoutRoot> */}
+      </DashboardClient>
     </>
   );
 };
