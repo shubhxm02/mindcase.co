fbbe6e76730b9edbf93b9b9c9710617801d1fd68
Shubham Gulati <shubham53.sg@gmail.com>
2023-09-20 20:55:10 +0530

    Add account menu in navbar

--- src/app/_layout/dashboard-navbar.tsx
+++ src/app/_layout/dashboard-navbar.tsx
@@ -2,15 +2,108 @@ import Link from "next/link";
 
 import Image from "next/image";
 
+import { useState } from "react";
+
 import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar, Avatar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { Menu, MenuItem, ListItemIcon, Divider } from "@mui/material";
+import { Settings, Logout } from "@mui/icons-material";
 
 import { Session } from "@supabase/supabase-js";
+
+
+const Account = ({ session }: { session: Session | null }) => {
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <IconButton onClick={handleClick}>
+        <Avatar sx={{ height: 40, width: 40 }} srcSet={session?.user?.user_metadata.avatar_url} />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            minWidth: "300px",
+            overflow: 'visible',
+            backgroundColor: 'background.paper',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 20,
+              width: 20,
+              height: 20,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <Typography sx={{ px: 2, py: 1.5, fontWeight: 600 }} variant="subtitle2">
+          {session?.user?.user_metadata.full_name}
+        </Typography>
+        <Typography sx={{ px: 2, pb: 1.5, color: 'text.secondary' }} variant="body2">
+          {session?.user?.email}
+        </Typography>
+        {/* <MenuItem onClick={handleClose}>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar /> My account
+        </MenuItem> */}
+        <Divider />
+        {/* menu item handleClose and route to /c */}
+        <Link href="/c" passHref>
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Settings fontSize="small" />
+            </ListItemIcon>
+            History
+          </MenuItem>
+        </Link>
+        <Link href="/logout" passHref>
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
+        </Link>
+      </Menu>
+    </>
+  )
+}
 
 
 export const DashboardNavbar = (
   { session, setSidebarOpen }: { session: Session | null, setSidebarOpen: () => void }
 ) => {
+
   return (
     <>
       <AppBar sx={{ width: 'full', backgroundColor: "primary", boxShadow: 'none', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
@@ -29,15 +122,7 @@ export const DashboardNavbar = (
           {/* Space */}
           <Box sx={{ flexGrow: 1 }} />
           {/* Space */}
-          <Button>
-            <Link href="/account" passHref>
-              {session ? (
-                <Avatar sx={{ height: 40, width: 40 }} srcSet={session.user?.user_metadata.avatar_url} />
-              ) : (
-                <Avatar sx={{ height: 40, width: 40 }} />
-              )}
-            </Link>
-          </Button>
+          <Account session={session} />
         </Toolbar>
       </AppBar>
     </>
